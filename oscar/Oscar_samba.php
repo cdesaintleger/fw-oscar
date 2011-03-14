8bc527d633bd2d07a12a17bdbaa6e10bc30d9ebd
christophe de saint leger <cdesaintleger@creavi.fr>
2011-03-14 19:05:26 +0100

    Config nouvelle version samba nosetuid

--- oscar/Oscar_samba.php
+++ oscar/Oscar_samba.php
@@ -132,7 +132,7 @@ class Oscar_samba{
             foreach( $this->_options AS $key => &$value ){
                 $option[] = $key."=".escapeshellarg($value);
             }
-            $options    = " -o ".implode(",", $option)." ".$params.",file_mode=0777,dir_mode=0777";
+            $options    = " -o ".implode(",", $option)." ".$params.",nosetuids,rw,nounix,nouser_xattr,noserverino,noperm";
 
             //La commande finale devient
             $cmd    =   "sudo /usr/bin/smbmount ".$this->_share." ".$this->_pmount." ".$options;
